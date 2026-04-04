c46cd414b31bfa726e63f81ca4e63c84c84c1bad
MarcoLenschau <contact@marco-lenschau.de>
2026-04-04 04:51:19 +0200

    chore: Safe the route with a api key

--- server.ts
+++ server.ts
@@ -9,6 +9,15 @@ app.listen(process.env.PORT, () => {
 });
 
 app.post("/api/email/send", (req: Request, res: Response) => {
+    const apiKeyHeader = req.headers["api-key"];
+    if (!apiKeyHeader) {
+        res.status(400).send("Bad Request: API Key header missing");
+        return;
+    }
+    if (!process.env.API_KEY?.split(',').map(key => key.trim()).filter(Boolean).includes(apiKeyHeader.toString())) {
+        res.status(403).send("Forbidden: Invalid API Key");
+        return;
+    }
     if (!req.body || !req.body.subject || !req.body.text || req.body.website) {
         res.status(400).send("Bad Request: Missing request body");
         return;
